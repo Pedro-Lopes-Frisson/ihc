496b5eab3e4253da22abc20df983b6cdf407dfbe
Pedro <pdfl@ua.pt>
2021-05-23 23:08:25 +0100

    Rebuild Home Screen

--- WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/home/FridgeFragment.java
+++ WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/home/FridgeFragment.java
@@ -4,21 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.whatsinmyfridge.ObjectDeclaration.Item;
+import com.example.whatsinmyfridge.ObjectDeclaration.ItemRecViewAdapter;
 import com.example.whatsinmyfridge.R;
+
+import java.util.ArrayList;
 
 public class FridgeFragment extends Fragment {
 
     private FridgeViewModel fridgeViewModel;
+    private RelativeLayout relativeLayout;
+    private RecyclerView recyclerView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,7 +35,7 @@ public class FridgeFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_fridge, container, false);
 
-        final TextView textView = root.findViewById(R.id.text_dashboard12);
+        final TextView textView = root.findViewById(R.id.text_Fridge);
 
         fridgeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -35,51 +43,26 @@ public class FridgeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        relativeLayout = root.findViewById(R.id.parentFridge);
+        recyclerView = relativeLayout.findViewById(R.id.ItemRecycler);
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item("Bife de Vaca", 1000, 2, 1, "Carne", getString(R.string.skyImg)));
+        items.add(new Item("Bife de Frango", 1001, 4, 1, "Carne", getString(R.string.skyImg)));
+        items.add(new Item("Arroz", 809, 5, 1, "Cereal", getString(R.string.skyImg)));
+        items.add(new Item("Massa", 1080, 9, 2, "Massa", getString(R.string.skyImg)));
+        items.add(new Item("Chocolate", 10, 1, 1, "Sobremesa", getString(R.string.skyImg)));
+
+        //
+
+        ItemRecViewAdapter itemRecViewAdapter = new ItemRecViewAdapter(root.getContext());
+        itemRecViewAdapter.setItems(items);
+
+        recyclerView.setAdapter(itemRecViewAdapter);
+        GridLayoutManager a = new GridLayoutManager(root.getContext(), 2);
+        recyclerView.setLayoutManager(a);
 
 
-        View line1 = root.findViewById(R.id.l1);
-        View line2 = root.findViewById(R.id.l2);
-        View line3 = root.findViewById(R.id.l3);
-
-        TextView t1 = line1.findViewById(R.id.editTextTextPersonName);
-        TextView t2 = line2.findViewById(R.id.editTextTextPersonName);
-        TextView t3 = line3.findViewById(R.id.editTextTextPersonName);
-
-        t1.setText("Cereais");
-        t2.setText("Carne");
-        t3.setText("Peixe");
-
-
-        TextView label1 = line1.findViewById(R.id.label1); //first label from first line
-        TextView label2 = line1.findViewById(R.id.label2);
-
-        TextView label3 = line2.findViewById(R.id.label1); //first label from first line
-        TextView label4 = line2.findViewById(R.id.label2);
-
-        PopupMenu myPopup = new PopupMenu(this.getContext(), root);
-        PopupWindow mp = new PopupWindow();
-        mp.setHeight(200);
-        mp.setWidth(200);
-
-
-        label1.setText("Chocapics");
-        label2.setText("Estrelitas");
-        label3.setText("Bife de vaca");
-        label4.setText("Bife de frango");
-
-        label1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myPopup.show();
-            }
-        });
-
-        View cardView = line1.findViewById(R.id.card0);
-        cardView.findViewById(R.id.card0).findViewById(R.id.img1).setOnClickListener(v -> {
-            t2.setText("App goes BRrrr");
-            myPopup.show();
-            mp.showAtLocation(root.findViewById(R.id.fridge), 20, 20, 20);
-        });
 
         return root;
     }
