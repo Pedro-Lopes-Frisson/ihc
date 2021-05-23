f5754beee6e7e4669347260f3b994ecbe0af0a61
Pedro <pdfl@ua.pt>
2021-05-23 23:53:13 +0100

    Cards are clickable next uo is creating a modal box

---
+++ WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ObjectDeclaration/ItemRecViewAdapter.java
@@ -0,0 +1,86 @@
+package com.example.whatsinmyfridge.ObjectDeclaration;
+
+import android.content.Context;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.whatsinmyfridge.R;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class ItemRecViewAdapter extends RecyclerView.Adapter<ItemRecViewAdapter.ViewHolder> {
+    private ArrayList<Item> items = new ArrayList<>();
+    private Context context;
+    private OnCardListener onCardListener;
+
+    public ItemRecViewAdapter(Context context, OnCardListener onCardListener) {
+        this.onCardListener = onCardListener;
+        this.context = context;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view, onCardListener);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.itemName.setText(items.get(position).getName());
+        Glide.with(context).asBitmap().load(items.get(position).getImage()).into(holder.itemImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public interface OnCardListener {
+        void onCardClick(int position);
+    }
+
+    // Something important stuff
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView itemName;
+        private ImageView itemImage;
+        OnCardListener onCardListener;
+
+        public ViewHolder(@NonNull View itemView, OnCardListener cardListener) {
+            super(itemView);
+            itemName = itemView.findViewById(R.id.md_description);
+            itemImage = itemView.findViewById(R.id.item_img);
+            itemView.setOnClickListener(this);
+            onCardListener = cardListener;
+        }
+
+        @Override
+        public void onClick(View v) {
+            onCardListener.onCardClick(getAdapterPosition());
+        }
+    }
+}
--- WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/home/FridgeFragment.java
+++ WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/home/FridgeFragment.java
@@ -1,12 +1,15 @@
 package com.example.whatsinmyfridge.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.proto.ProtoOutputStream;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,17 +19,19 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.whatsinmyfridge.MainActivity;
 import com.example.whatsinmyfridge.ObjectDeclaration.Item;
 import com.example.whatsinmyfridge.ObjectDeclaration.ItemRecViewAdapter;
 import com.example.whatsinmyfridge.R;
 
 import java.util.ArrayList;
 
-public class FridgeFragment extends Fragment {
+public class FridgeFragment extends Fragment implements ItemRecViewAdapter.OnCardListener {
 
     private FridgeViewModel fridgeViewModel;
     private RelativeLayout relativeLayout;
     private RecyclerView recyclerView;
+    ArrayList<Item> items = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -46,7 +51,6 @@ public class FridgeFragment extends Fragment {
         relativeLayout = root.findViewById(R.id.parentFridge);
         recyclerView = relativeLayout.findViewById(R.id.ItemRecycler);
 
-        ArrayList<Item> items = new ArrayList<>();
         items.add(new Item("Bife de Vaca", 1000, 2, 1, "Carne", getString(R.string.skyImg)));
         items.add(new Item("Bife de Frango", 1001, 4, 1, "Carne", getString(R.string.skyImg)));
         items.add(new Item("Arroz", 809, 5, 1, "Cereal", getString(R.string.skyImg)));
@@ -55,7 +59,7 @@ public class FridgeFragment extends Fragment {
 
         //
 
-        ItemRecViewAdapter itemRecViewAdapter = new ItemRecViewAdapter(root.getContext());
+        ItemRecViewAdapter itemRecViewAdapter = new ItemRecViewAdapter(root.getContext(), this);
         itemRecViewAdapter.setItems(items);
 
         recyclerView.setAdapter(itemRecViewAdapter);
@@ -63,7 +67,12 @@ public class FridgeFragment extends Fragment {
         recyclerView.setLayoutManager(a);
 
 
-
         return root;
+    }
+
+    @Override
+    public void onCardClick(int position) {
+        items.get(position);
+        Toast.makeText(this.getContext(), "Hello " + items, Toast.LENGTH_LONG).show();
     }
 }
