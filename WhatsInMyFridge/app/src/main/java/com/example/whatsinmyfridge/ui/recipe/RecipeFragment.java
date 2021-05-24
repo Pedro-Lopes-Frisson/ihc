b19606dc6f280b4754aa2d4033ba2f99aa11d3f0
goncalo-machado <goncalofmachado01@gmail.com>
2021-05-24 14:36:31 +0100

    Made RecyclerView with cards, not done

--- WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/recipe/RecipeFragment.java
+++ WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/recipe/RecipeFragment.java
@@ -11,12 +11,19 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.whatsinmyfridge.R;
+
+import java.util.ArrayList;
 
 public class RecipeFragment extends Fragment {
 
     private RecipeViewModel recipeViewModel;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,12 +31,18 @@ public class RecipeFragment extends Fragment {
                 new ViewModelProvider(this).get(RecipeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_recipe, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        recipeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        ArrayList<RecipeCard> recipeCards = new ArrayList<>();
+        recipeCards.add(new RecipeCard(R.drawable.beef_com_arroz,"Beef with rice"));
+        recipeCards.add(new RecipeCard(R.drawable.beef_com_arroz,"Beef with rice2"));
+        recipeCards.add(new RecipeCard(R.drawable.beef_com_arroz,"Beef with rice3"));
+
+        mRecyclerView = root.findViewById(R.id.recyclerView);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mAdapter = new RecipeAdapter(recipeCards);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
 
         return root;
     }
