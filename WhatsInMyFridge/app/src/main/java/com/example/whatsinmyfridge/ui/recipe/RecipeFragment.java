d6364fd78f204ee438ceeb850d2fa4d9293e1bf0
goncalo-machado <goncalofmachado01@gmail.com>
2021-05-24 14:52:21 +0100

    2 Recipes per row

--- WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/recipe/RecipeFragment.java
+++ WhatsInMyFridge/app/src/main/java/com/example/whatsinmyfridge/ui/recipe/RecipeFragment.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -38,7 +39,7 @@ public class RecipeFragment extends Fragment {
         recipeCards.add(new RecipeCard(R.drawable.beef_com_arroz,"Beef with rice3"));
 
         mRecyclerView = root.findViewById(R.id.recyclerView);
-        mLayoutManager = new LinearLayoutManager(getContext());
+        mLayoutManager = new GridLayoutManager(getContext(), 2);
         mAdapter = new RecipeAdapter(recipeCards);
 
         mRecyclerView.setLayoutManager(mLayoutManager);
